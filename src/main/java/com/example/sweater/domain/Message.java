0845078ccffa2b8ca557b031d0d5a68da8e888ad
mikha <mixa199525@yandex.ru>
2022-02-15 21:15:27 +0300

    [004] -> Bean validation, шифрование паролей

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,6 +1,9 @@
 package com.example.sweater.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
@@ -9,8 +12,10 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @NotBlank(message = "Please fill the message!")
+    @Length(max = 2048, message = "Message too long (More then 2kB)!")
     private String text;
-
+    @Length(max = 255, message = "Tag too long (More then 2kB)!")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
